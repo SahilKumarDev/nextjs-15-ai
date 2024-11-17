6a4e8290d12f85e89f94a1ae1fb5d7325860121b
sahilkumardev <sahilkumar710@outlook.com>
2024-11-17 12:55:46 +0530

    add coustom scrollbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { ModeToggle } from "@/components/ModeToggle";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen w-screen gap-4">
+    <div className="flex justify-center items-center h-screen gap-4">
       <h1>Create next app</h1>
       <ModeToggle />
     </div>
